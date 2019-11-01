2d2a162fff491358699f11f7ffd4702e039ca3b9
Mengyu Gong <mengyugong@Mengyus-MBP.zyxel.com>
2019-10-31 21:06:45 -0400

    mengyu first commit

--- src/main/java/com/example/demo/Controller/PersonController.java
+++ src/main/java/com/example/demo/Controller/PersonController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @Component
 @CrossOrigin
 public class PersonController {
-
+//mengyu wants to collaborate
     @Autowired
     private PersonService personService;
 
